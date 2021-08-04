07f0232ed71e876aaf25382078b7d22f6dbe0780
regular-alves <rodrigo.gois@live.com>
2021-08-03 21:28:29 -0300

    add: create method to populate sudoku

--- database/factories/SudokuFactory.php
+++ database/factories/SudokuFactory.php
@@ -26,6 +26,75 @@ class SudokuFactory extends Factory
         ];
     }
 
+    public function configure()
+    {
+        return $this->afterMaking(fn (Sudoku $sudoku) => $this->polulate($sudoku));
+    }
+
+    public function polulate(Sudoku $sudoku)
+    {
+        $positions = $sudoku->positions;
+
+        $section_length = 3;
+        $board_length = pow($section_length, 2);
+
+        $fields = range(0, pow($board_length, 2) - 1);
+        $possible_num = range(1, $board_length);
+
+        $letter = 'a';
+        $columns = [];
+
+        for ($i = 0; $i < $board_length; $i++) {
+            $columns[] = $letter++;
+        }
+
+        shuffle($fields);
+
+        foreach ($fields as $n) {
+            unset(
+                $row_n,
+                $col_n,
+                $sec_row_start,
+                $sec_row_end,
+                $sec_col_start,
+                $sec_col_end,
+                $setted_num,
+                $remain,
+                $key
+            );
+
+            $row_n = floor($n / $board_length);
+            $col_n = $n % $board_length;
+
+            $sec_row_start = floor($row_n / $section_length);
+            $sec_row_end = (($sec_row_start + 1) * $section_length) - 1;
+
+            $sec_col_start = floor($col_n / $section_length);
+            $sec_col_end = (($sec_col_start + 1) * $section_length) - 1;
+
+            $setted_num = array_merge(
+                $sudoku->getRow($row_n, true),
+                $sudoku->getColumn($columns[$col_n], true),
+                $sudoku->getSection(
+                    $sec_row_start,
+                    $columns[$sec_col_start],
+                    $sec_row_end,
+                    $columns[$sec_col_end],
+                    true
+                )
+            );
+
+            $remain = array_diff($possible_num, $setted_num);
+            $key = array_rand($remain);
+
+            $positions[$row_n][$columns[$col_n]] = $remain[$key];
+        }
+
+        $sudoku->setPositions($positions);
+
+        return $sudoku;
+    }
+
     public function getPositions(): array
     {
         $columns = $rows = [];
